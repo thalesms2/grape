5ff73eb1dcc167f736675640c55794b5505b914b
Thales Minoru Sato <thalesms2@outlook.com>
2023-05-03 15:13:26 -0300

    feat: create model usuario

---
+++ restApi/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace restApi.Models; 
+public class Usuario {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Type { get; set; }
+    public string Organization { get; set; }
+    public string Telephone { get; set; }
+}
